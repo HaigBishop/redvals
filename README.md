2bd6e5719fa64ba91799da6f0cb2ce88411de271
HaigBishop <haigvbishop@gmail.com>
2025-03-27 13:26:09 +1300

    better examples and readme

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Tool for obtaining RED values from GTDB phylogenetic trees
 
 
-## Usage
+
+
+## Set Up
 1. Clone repository
 ```
 git clone https://github.com/HaigBishop/redvals.git
@@ -14,19 +16,61 @@ conda install -c conda-forge biopython
 conda install -c conda-forge pandas
 conda install -c conda-forge tqdm
 ```
-3. Use package! 
-e.g. Decorate trees from scratch with `example_1.py`
+3. Use package
+ - See usage below
+ - Or see in depth examples in `example_1.py` and `example_2.py`
+
+
+## Usage
+#### Import redvals
+```python
+from redvals import RedTree
 ```
-python example_1.py
+
+#### Load Undecorated Trees
+```python
+red_trees = RedTree("trees/bac120_r220.tree", "trees/ar53_r220.tree")
 ```
-or e.g. Load already decorated trees with `example_2.py`
+
+#### Decorate Trees from TSVs
+```python
+red_trees.decorate_from_tsv("red_values/gtdbtk_r220_bac120.tsv", "red_values/gtdbtk_r220_ar53.tsv")
 ```
-python example_2.py
+
+#### Write Decorated Trees
+```python
+red_trees.write_decorated_trees("decorated_trees/bac120_r220_decorated.pkl", "decorated_trees/ar53_r220_decorated.pkl")
+```
+
+#### Load Decorated Trees
+```python
+red_trees = RedTree("decorated_trees/bac120_r220_decorated.pkl", "decorated_trees/ar53_r220_decorated.pkl")
+```
+
+#### Convert Node IDs
+```python
+bacterial_redvals_id = red_trees.get_redvals_id("GB_GCA_002687935.1")
+gtdb_id = red_trees.get_gtdb_id("bac00000001")
+```
+
+#### Access Node Info
+```python
+node_info = red_trees.get_node_info("bac00000001")
+print(f"RED value: {node_info.red_value}, Domain: {node_info.domain}")
+```
+
+#### Compute RED Distances
+```python
+red_distance, mrca_node_id = red_trees.dist_between_nodes("bac00000001", "RS_GCF_001186155.3")
 ```
 
 
-## Original Newick Tree Files
-#### ./trees/{DOMAIN}_r220.tree
+
+
+## Files
+
+### Original Newick Tree Files
+#### ./trees/bac120_r220.tree and ./trees/ar53_r220.tree
 These are the GTDB phylogenetic trees (release 220) in Newick format. Obtained from: https://gtdb.ecogenomic.org/downloads
 
 Example usage:
@@ -43,15 +87,15 @@ print("Number of leaf nodes:", len(terminal_nodes))
 print("Number of internal nodes:", len(nonterminal_nodes))
 ```
 
-## RED Values TSV Files
-#### ./red_values/gtdbtk_r220_{DOMAIN}.tsv
+### RED Values TSV Files
+#### ./red_values/gtdbtk_r220_bac120.tsv and ./red_values/gtdbtk_r220_ar53.tsv
 These TSV files contain the RED values for all nodes in each tree. They originate from the release 220 gtdbtk_package directory from: https://gtdb.ecogenomic.org/downloads
 i.e. https://data.ace.uq.edu.au/public/gtdb/data/releases/release220/220.0/auxillary_files/gtdbtk_package/full_package/
 
 There is one row for each node (terminal and nonterminal). The second column holds the RED value assigned to the given node. The first column holds one or two leaf IDs (AKA genome IDs). For example in the first column we might find "GB_GCA_020721905.1" or "GB_GCA_026414805.1|GB_GCA_020721905.1". When there is a single leaf ID, this is a leaf node, and therefore the RED value in column two is 1.0. But if there are two leaf IDs seperated by a "|" symbol, this row represents an internal node. Specifically, it represents the node that is the most recent common ancestor (MRCA) of the two leaf IDs in column one.
 
-## Decorated Tree Files
-#### ./decorated_trees/{DOMAIN}_r220_decorated.pkl
+### Decorated Tree Files
+#### ./decorated_trees/bac120_r220_decorated.pkl and ./decorated_trees/ar53_r220_decorated.pkl
 These files are Python pickle files, each containing a Bio.Phylo.Newick.Tree object. Loading these objects (see below) results in the same object as loading the .tree files (as above), but the difference is that they are decorated with RED values, and [TODO].
 
 Example usage:
@@ -67,7 +111,7 @@ with open("./out/bac120_r220_decorated.pkl", "rb") as f:
 ```
 
 
-#### Phylogenetic tree diagram for reference:
+### Reference Diagram:
 ![Phylogenetic tree visualization](res/tree.png)
 
 
--- example_2.py
+++ example_2.py
@@ -24,7 +24,7 @@ BAC_DECORATED_TREE_PATH = "decorated_trees/bac120_r220_decorated.pkl"
 red_trees = RedTree(BAC_DECORATED_TREE_PATH, ARC_DECORATED_TREE_PATH)
 
 # The trees are already decorated with RED values
-print("\nAre the trees decorated:", red_trees.is_decorated())
+print("Are the trees decorated:", red_trees.is_decorated())
 
 
 # 2. Convert between different node ID formats -----------
@@ -53,18 +53,7 @@ print(f"Redvals ID '{redvals_id}' corresponds to GTDB ID '{gtdb_id}'")
 # Get information for a specific node
 redvals_id = "bac00000001"
 node_info = red_trees.get_node_info(redvals_id)
-print(f"\nNode {redvals_id}:")
-print(f"  redvals ID: {node_info.redvals_id}")
-print(f"  GTDB ID: {node_info.gtdb_id}")
-print(f"  Domain: {node_info.domain}")
-print(f"  RED value: {node_info.red_value}")
-print(f"  RED distance: {node_info.red_distance}")
-print(f"  Is terminal node: {node_info.is_terminal}")
-
-# Get information for a specific node
-redvals_id = "bac00002898"
-node_info = red_trees.get_node_info(redvals_id)
-print(f"\nNode {redvals_id}:")
+print(f"\nNode {redvals_id} Info:")
 print(f"  redvals ID: {node_info.redvals_id}")
 print(f"  GTDB ID: {node_info.gtdb_id}")
 print(f"  Domain: {node_info.domain}")
@@ -75,7 +64,18 @@ print(f"  Is terminal node: {node_info.is_terminal}")
 # Get information for a specific node
 redvals_id = "GB_GCA_028725265.1"
 node_info = red_trees.get_node_info(redvals_id)
-print(f"\nNode {redvals_id}:")
+print(f"\nNode {redvals_id} Info:")
+print(f"  redvals ID: {node_info.redvals_id}")
+print(f"  GTDB ID: {node_info.gtdb_id}")
+print(f"  Domain: {node_info.domain}")
+print(f"  RED value: {node_info.red_value}")
+print(f"  RED distance: {node_info.red_distance}")
+print(f"  Is terminal node: {node_info.is_terminal}")
+
+# Get information for a specific node
+redvals_id = "bac00152898"
+node_info = red_trees.get_node_info(redvals_id)
+print(f"\nNode {redvals_id} Info:")
 print(f"  redvals ID: {node_info.redvals_id}")
 print(f"  GTDB ID: {node_info.gtdb_id}")
 print(f"  Domain: {node_info.domain}")
@@ -86,7 +86,7 @@ print(f"  Is terminal node: {node_info.is_terminal}")
 # Get information for a specific node
 redvals_id = "arc00002281"
 node_info = red_trees.get_node_info(redvals_id)
-print(f"\nNode {redvals_id}:")
+print(f"\nNode {redvals_id} Info:")
 print(f"  redvals ID: {node_info.redvals_id}")
 print(f"  GTDB ID: {node_info.gtdb_id}")
 print(f"  Domain: {node_info.domain}")
@@ -95,9 +95,9 @@ print(f"  RED distance: {node_info.red_distance}")
 print(f"  Is terminal node: {node_info.is_terminal}")
 
 # Get information for a specific node
-redvals_id = "RS_GCF_000337735.1"
+redvals_id = "arc00007699"
 node_info = red_trees.get_node_info(redvals_id)
-print(f"\nNode {redvals_id}:")
+print(f"\nNode {redvals_id} Info:")
 print(f"  redvals ID: {node_info.redvals_id}")
 print(f"  GTDB ID: {node_info.gtdb_id}")
 print(f"  Domain: {node_info.domain}")
@@ -107,20 +107,42 @@ print(f"  Is terminal node: {node_info.is_terminal}")
 
 
 # 4. Calculate RED distances between pairs of nodes -----------
-# Example node pairs
-node_pairs = [
-    ("bac00000001", "bac00000002"),  # Two XXX nodes
-    ("bac00090342", "bac00054321"),  # Two XXX nodes
-    ("arc00000147", "arc00002342"),  # Two XXX nodes
-]
+# Get distance between two leaf nodes
+leaf_node_1 = "bac00000001"
+leaf_node_2 = "RS_GCF_001186155.3"
+red_distance, mrca_node_id = red_trees.dist_between_nodes(leaf_node_1, leaf_node_2)
+print("\nTwo Leaf Nodes:")
+print(f"The RED distance between {leaf_node_1} and {leaf_node_2} is {red_distance:.6f}")
+# Get the MRCA node information
+mrca_node_info = red_trees.get_node_info(mrca_node_id)
+print(f"Their MRCA node is: {mrca_node_id} with RED value: {mrca_node_info.red_value:.6f}")
 
-for node1_id, node2_id in node_pairs:
-    # Calculate RED distance between nodes
-    red_distance, mrca_node_id = red_trees.dist_between_nodes(node1_id, node2_id)
+# Get distance between a leaf and an internal node
+leaf_node = "GB_GCA_028725265.1"
+internal_node = "bac00152898"
+red_distance, mrca_node_id = red_trees.dist_between_nodes(leaf_node, internal_node)
+print(f"\nLeaf and Internal Node:")
+print(f"The RED distance between {leaf_node} and {internal_node} is {red_distance:.6f}")
+# Get the MRCA node information
+mrca_node_info = red_trees.get_node_info(mrca_node_id)
+print(f"Their MRCA node is: {mrca_node_id} with RED value: {mrca_node_info.red_value:.6f}")
 
-    # Get the MRCA node information
-    mrca_node_info = red_trees.get_node_info(mrca_node_id)
-    
-    print(f"\nThe RED distance between {node1_id} and {node2_id} is {red_distance:.6f}")
-    print(f"Their MRCA node is: {mrca_node_id} with RED value: {mrca_node_info.red_value:.6f}")
+# Get distance between two internal nodes
+internal_node_1 = "arc00007699"
+internal_node_2 = "arc00007700"
+red_distance, mrca_node_id = red_trees.dist_between_nodes(internal_node_1, internal_node_2)
+print("\nTwo Internal Nodes:")
+print(f"The RED distance between {internal_node_1} and {internal_node_2} is {red_distance:.6f}")
+# Get the MRCA node information
+mrca_node_info = red_trees.get_node_info(mrca_node_id)
+print(f"Their MRCA node is: {mrca_node_id} with RED value: {mrca_node_info.red_value:.6f}")
+
+# Get distance between identical nodes
+node_id = "bac00000420"
+red_distance, mrca_node_id = red_trees.dist_between_nodes(node_id, node_id)
+print("\nIdentical Nodes:")
+print(f"The RED distance between {node_id} and {node_id} is {red_distance:.6f}")
+# Get the MRCA node information
+mrca_node_info = red_trees.get_node_info(mrca_node_id)
+print(f"Their MRCA node is: {mrca_node_id} with RED value: {mrca_node_info.red_value:.6f}")
 
